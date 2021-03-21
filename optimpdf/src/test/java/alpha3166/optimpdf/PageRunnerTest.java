537997e395c1b7f6c393e119065f838e9c071a13
alpha3166 <foxtrot3166-git@yahoo.co.jp>
2021-03-21 13:42:49 +0900

    Fix to use temp dir for testing

--- optimpdf/src/test/java/alpha3166/optimpdf/PageRunnerTest.java
+++ optimpdf/src/test/java/alpha3166/optimpdf/PageRunnerTest.java
@@ -2,16 +2,27 @@ package alpha3166.optimpdf;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PageRunnerTest {
+	Path base;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		base = Files.createTempDirectory(Paths.get(""), "junit");
+	}
+
 	@AfterEach
 	void tearDown() throws Exception {
-		Files.deleteIfExists(Paths.get("src.pdf"));
+		Files.walk(base).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
 	}
 
 	@Test
@@ -37,9 +48,9 @@ class PageRunnerTest {
 	void test(int srcWidth, int srcHeight, int expectedWidth, int expectedHeight) throws Exception {
 		// Setup
 		var pdf = ImageMagickHelper.exec(String.format("wizard: -resize %dx%d! pdf:-", srcWidth, srcHeight));
-		Files.write(Paths.get("src.pdf"), pdf);
-		var pdfHandler = new PdfHandler(Paths.get("src.pdf"));
-		var optHandler = new OptionHandler("src.pdf");
+		Files.write(base.resolve("src.pdf"), pdf);
+		var pdfHandler = new PdfHandler(base.resolve("src.pdf"));
+		var optHandler = new OptionHandler(base + "/src.pdf");
 		// Exercise
 		var pageRunner = new PageRunner(pdfHandler, 1, optHandler);
 		pageRunner.run();
--- optimpdf/src/test/java/alpha3166/optimpdf/PdfHandlerTest.java
+++ optimpdf/src/test/java/alpha3166/optimpdf/PdfHandlerTest.java
@@ -3,8 +3,11 @@ package alpha3166.optimpdf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.io.File;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -13,27 +16,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PdfHandlerTest {
+	static Path base;
 	PdfHandler sut;
 
 	@BeforeAll
 	static void setUpClass() throws Exception {
+		base = Files.createTempDirectory(Paths.get(""), "junit");
 		byte[] srcPdf = ImageMagickHelper.exec("wizard: pdf:-");
-		Files.write(Paths.get("src.pdf"), srcPdf);
+		Files.write(base.resolve("src.pdf"), srcPdf);
 	}
 
 	@AfterAll
 	static void tearDownClass() throws Exception {
-		Files.delete(Paths.get("src.pdf"));
+		Files.walk(base).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
-		sut = new PdfHandler(Paths.get("src.pdf"));
+		sut = new PdfHandler(base.resolve("src.pdf"));
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		Files.deleteIfExists(Paths.get("dest.pdf"));
+		Files.deleteIfExists(base.resolve("dest.pdf"));
 	}
 
 	@Test
@@ -67,9 +72,9 @@ class PdfHandlerTest {
 	@Test
 	void testSave() throws Exception {
 		// Exercise
-		sut.save(Paths.get("dest.pdf"));
+		sut.save(base.resolve("dest.pdf"));
 		// Verify
-		var pdfHandler = new PdfHandler(Paths.get("dest.pdf"));
+		var pdfHandler = new PdfHandler(base.resolve("dest.pdf"));
 		var jpegHandler = new JpegHandler(pdfHandler.extractJpeg(1));
 		assertEquals(480, jpegHandler.getWidth());
 		assertEquals(640, jpegHandler.getHeight());
